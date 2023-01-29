9de5fc55cc8812bbfa3d37e608887de4075bacdb
Auctoritas <auctoritasthebold@hotmail.com>
2023-01-29 14:22:20 +0100

    finally got dialog working right

--- Dialog/DialogWitch.gd
+++ Dialog/DialogWitch.gd
@@ -5,13 +5,19 @@ export(String, FILE, "*.json") var d_file
 var dialog = []
 var current_dialog_id = 0
 var d_active = false
+var interactCount = 0
 
 var textures = {}
+
+var initial_dialog = []
+var ingredients = []
 
 var witch1 = load("res://art/witch/lul.png")
 var witch2 = load("res://art/witch/knife.png")
 var witch3 = load("res://art/witch/cash.png")
 var witch4 = load("res://art/witch/muah.png")
+
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -27,18 +33,15 @@ func start():
 	if d_active:
 		return
 	d_active = true
-	$NinePatchRect.visible = true
-	
 	dialog = load_dialog()
-	$NinePatchRect/Name.text = dialog[0]['name']
-	$NinePatchRect/Text.text = dialog[0]['text']
-	$NinePatchRect/Sprite.texture = textures[dialog[0]['emotion']]
+	initial_dialog = dialog['initial_dialog']
+	ingredients = dialog['ingredients']
+	$NinePatchRect.visible = true
+	if current_dialog_id == 0:
+		$NinePatchRect/Name.text = initial_dialog[0]['name']
+		$NinePatchRect/Text.text = initial_dialog[0]['text']
+		$NinePatchRect/Sprite.texture = textures[initial_dialog[0]['emotion']]
 
-#func getTexture(emote):
-#	var emo = emote
-#	var texture = Texture.new()
-#	texture.load(emo)
-#	$NinePatchRect/Sprite.texture = texture
 
 func load_dialog():
 	var file = File.new()
@@ -53,16 +56,39 @@ func _input(event):
 		next_script()
 		
 func next_script():
-	current_dialog_id += 1
+	if interactCount < 1:
+		if (current_dialog_id) >= len(initial_dialog)-1:
+			$Timer.start()
+			$NinePatchRect.visible = false
+			interactCount += 1
+		else:
+			current_dialog_id += 1
+			$NinePatchRect.visible = true
+			$NinePatchRect/Name.text = initial_dialog[current_dialog_id]['name']
+			$NinePatchRect/Text.text = initial_dialog[current_dialog_id]['text']
+			$NinePatchRect/Sprite.texture = textures[initial_dialog[current_dialog_id]['emotion']]
+			print(current_dialog_id)
+
+
+	if interactCount >= 1:
+		$NinePatchRect.visible = true
+		current_dialog_id = 0
+		var randoRequest = randi() % ingredients.size()
+		$NinePatchRect/Name.text = ingredients[randoRequest]['name']
+		$NinePatchRect/Text.text = ingredients[randoRequest]['text']
+		$NinePatchRect/Sprite.texture = textures[ingredients[randoRequest]['emotion']]
+		current_dialog_id += 1
+		print(current_dialog_id)
+		if current_dialog_id >= 1:
+			$Timer.start()
+			$NinePatchRect.visible = false
+		
+#	$NinePatchRect.visible = true
+
 	
-	if current_dialog_id >= len(dialog):
-		$Timer.start()
-		$NinePatchRect.visible = false
-		return
-	
-	$NinePatchRect/Name.text = dialog[current_dialog_id]['name']
-	$NinePatchRect/Text.text = dialog[current_dialog_id]['text']
-	$NinePatchRect/Sprite.texture = textures[dialog[current_dialog_id]['emotion']]
+#	$NinePatchRect/Name.text = dialog[current_dialog_id]['name']
+#	$NinePatchRect/Text.text = dialog[current_dialog_id]['text']
+#	$NinePatchRect/Sprite.texture = textures[dialog[current_dialog_id]['emotion']]
 	
 	
 
