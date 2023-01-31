f71ec81b40c2ec3185fa9281e7824eaa92331c1a
Kurzweil <m.barr@mac.com>
2023-01-31 21:44:58 +0100

    Basic tool pickups and usage

--- objects/Object.gd
+++ objects/Object.gd
@@ -1,8 +1,18 @@
 extends StaticBody2D
 
+export(String) var tool_name
+
+func _ready():
+	$item_label.text = tool_name
+
 func _interact():
-	print("Interacted")
+	if tool_name: TempInventory.playerinv[tool_name] = true
+	else:
+		print("Nothing to pickup")
+		return
+	print(TempInventory.playerinv)
 	$debug_label.show()
 	$InteractTimer.start()
 	yield($InteractTimer, "timeout")
 	$debug_label.hide()
+	queue_free()
